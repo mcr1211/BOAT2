7023acc4d02d41299a3a6e6f4b94b60204c2fbb9
Toni Dalmau Martínez <Toni Dalmau Martnez@PINGU>
2016-05-25 01:15:53 +0200

    Projecte Modificat

--- src/com/BoatToni/Persona/Taller.java
+++ src/com/BoatToni/Persona/Taller.java
@@ -3,6 +3,7 @@ package com.BoatToni.Persona;
 import com.BoatToni.Empresa.Empresa;
 import com.BoatToni.Exceptions.EmpleatException;
 import com.BoatToni.Exceptions.PersonaException;
+import com.BoatToni.Exceptions.TallerException;
 import com.BoatToni.Operacions.Venda;
 import com.BoatToni.Vaixell.Model;
 import java.util.ArrayList;
@@ -26,10 +27,30 @@ public class Taller extends Empleat {
         souTaller = empleat.getSouEmpleat();
         return souTaller;
     }
+    
+    public void afegirSkill (Habilitat h) throws TallerException{
+        if (skill.lastIndexOf(h)!=-1){
+            throw new TallerException("L'habilitat no s'ha afegit.");
+        }else {
+            skill.add(h);
+        }
+    }
+    
+    public void eliminarSkill (Habilitat h) throws TallerException{
+        if (skill.contains(h)){
+            skill.remove(h);
+        }else {
+            throw new TallerException("L'habilitat no s'ha borrat.");
+        }
+    }
+    
+    public ArrayList mostraSkills(){
+        return skill;
+    }
 
     @Override
     public String toString() {
-        return "Taller{"+ "nom= " + nom + "llinatges= " + llinatges + "tipusDocument= " + document + "numDocument= " + numDocument + "adreça= " + adreça + "email= " + email + "telefon= " + telefon + "dataAlta= " + dataAlta + "souTaller= " + souTaller + '}';
+        return "Taller{"+ "nom= " + nom + "llinatges= " + llinatges + "tipusDocument= " + document + "numDocument= " + numDocument + "adreça= " + adreça + "email= " + email + "telefon= " + telefon + "dataAlta= " + dataAlta + "Habilitat/s= " + skill + "souTaller= " + souTaller + '}';
     }
     
     
